7acc3e82228ca706e90a8ad7b24e1bc9806a1240
Hao, Simon Xingzhi (901021474) <shao1@wpi.edu>
2026-05-17 23:23:16 +0000

    Upload New File

---
+++ MeshEngine/mesh_tricenter.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def mesh_tricenter(P, t):
+#  SYNTAX
+#  C = meshtricenter(P, t) 
+#   DESCRIPTION
+#   This function returns triangle centers (a Nx3 array)
+#
+#   To display the mesh use: fv.faces = t; fv.vertices = P;
+#   patch(fv, 'FaceColor', 'y'); axis equal; view(160, 60); grid on;
+#
+#   Low-Frequency Electromagnetic Modeling for Electrical and Biological
+#   Systems Using MATLAB, Sergey N. Makarov, Gregory M. Noetscher, and Ara
+#   Nazarian, Wiley, New York, 2015, 1st ed.
+
+    C = (P[t[:, 0], :] + P[t[:, 1], :] + P[t[:, 2], :]) / 3.0
+    return C
