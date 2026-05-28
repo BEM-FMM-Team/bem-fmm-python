3e29e55e5b04c37c6680de51ce5bb1fb767e4300
Hao, Simon Xingzhi (901021474) <shao1@wpi.edu>
2026-05-28 19:59:04 +0000

    Upload New File

---
+++ engines/mesh/mesh_laplace_normals.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def mesh_laplace_normals(P, t, normals):
+#   SYNTAX
+#   unitnormals = meshnormals(P, t, normals)
+#   DESCRIPTION
+#   For a manifold mesh P, t, this function returns an array of outer
+#   normal vectors for each triangle with correction
+#   Inputs:
+#   P - Vertex array of the mesh (N x 3)
+#   t - Facets of the mesh (N x 3)
+
+    vert1 = P[t[:, 0], :]
+    vert2 = P[t[:, 1], :]
+    vert3 = P[t[:, 2], :]
+    # Finding edges
+    edge1 = vert2 - vert1
+    edge2 = vert3 - vert1
+
+    normal = np.cross(edge1, edge2)
+    length = np.sqrt(
+        normal[:, 0]**2 +
+        normal[:, 1]**2 +
+        normal[:, 2]**2
+        )
+    unitnormals = normal / length[:, None]
+    correction = np.sum(unitnormals * normals, axis = 1)
+    correction = correction[:, None]
+    unitnormals = correction * unitnormals
+
+    return unitnormals
