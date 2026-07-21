067e5bfb4d0de8e45bf7c239782ff58d8d7bff95
Hao, Simon Xingzhi (901021474) <shao1@wpi.edu>
2026-07-21 18:48:47 +0000

    Update 3 files
    - /engines/gui/gui_frontend.py
    - /engines/gui/ui_main_window.py
    - /engines/gui/gui_frontend_new.py

--- engines/gui/gui_frontend_new.py
+++ engines/gui/gui_frontend_new.py
@@ -11,7 +11,6 @@ from engines.gui.ui_main_window import Ui_MainWindow
 GUI frontend for placing coils. This class is responsible for managing the widget.
 """
 
-
 class Frontend(QMainWindow):
     def __init__(self, head_models, names):
         super().__init__()
@@ -27,7 +26,7 @@ class Frontend(QMainWindow):
         self.ui.setupUi(self)
 
         # backend
-        self.backend = Backend(head_models, self.ui.ViewPort)
+        self.backend = Backend(head_models,self.ui.ViewPort)
 
         self.setupUi()
         self.backend.renderer.activate()
@@ -39,17 +38,12 @@ class Frontend(QMainWindow):
         self.ui.TypeDropdown.clear()
         for name in self.coil_names:
             self.ui.TypeDropdown.addItem(name)
-
-        for slider in (self.ui.XSlider, self.ui.YSlider, self.ui.ZSlider):
+        
+        for slider in (self.ui.XSlider,self.ui.YSlider,self.ui.ZSlider):
             slider.setMinimum(-150000)
             slider.setMaximum(150000)
-
-        for slider in (
-            self.ui.rXSlider,
-            self.ui.rYSlider,
-            self.ui.rZSlider,
-            self.ui.TwistSlider,
-        ):
+    
+        for slider in (self.ui.rXSlider,self.ui.rYSlider,self.ui.rZSlider,self.ui.TwistSlider):
             slider.setMinimum(-180000)
             slider.setMaximum(180000)
 
@@ -60,6 +54,7 @@ class Frontend(QMainWindow):
         self.ui.Delete.clicked.connect(self.delete_selected_coil)
         self.ui.Edit.clicked.connect(self.edit_selected_coil)
         self.ui.Undo.clicked.connect(self.undo)
+        self.ui.Save.clicked.connect(self.backend.save_coil_config)
         self.ui.ApplyHeadModel.clicked.connect(self.apply_head_models)
         self.ui.Skin.setChecked(True)
 
@@ -77,59 +72,39 @@ class Frontend(QMainWindow):
 
         self.ui.WhiteMatterButton.clicked.connect(self.place_with_white_matter)
 
-        for box in (
-            self.ui.XEntry,
-            self.ui.YEntry,
-            self.ui.ZEntry,
-        ):
-            box.setRange(-0.15, 0.15)
+        for box in (self.ui.XEntry,self.ui.YEntry,self.ui.ZEntry,):
+            box.setRange(-150.0, 150.0)
             box.setDecimals(4)
             box.setSingleStep(0.0001)
 
-        for box in (
-            self.ui.rXEntry,
-            self.ui.rYEntry,
-            self.ui.rZEntry,
-            self.ui.TwistEntry,
-        ):
+        for box in (self.ui.rXEntry,self.ui.rYEntry,self.ui.rZEntry,self.ui.TwistEntry,):
             box.setRange(-180.0, 180.0)
             box.setDecimals(3)
             box.setSingleStep(0.1)
-
-        POSITION_SCALE = 1000000
+        POSITION_SCALE = 1000
         ROTATION_SCALE = 1000
-
-        self.bind_slider_spinbox(self.ui.XSlider, self.ui.XEntry, POSITION_SCALE)
-        self.bind_slider_spinbox(self.ui.YSlider, self.ui.YEntry, POSITION_SCALE)
-        self.bind_slider_spinbox(self.ui.ZSlider, self.ui.ZEntry, POSITION_SCALE)
-
-        self.bind_slider_spinbox(self.ui.rXSlider, self.ui.rXEntry, ROTATION_SCALE)
-        self.bind_slider_spinbox(self.ui.rYSlider, self.ui.rYEntry, ROTATION_SCALE)
-        self.bind_slider_spinbox(self.ui.rZSlider, self.ui.rZEntry, ROTATION_SCALE)
-        self.bind_slider_spinbox(
-            self.ui.TwistSlider, self.ui.TwistEntry, ROTATION_SCALE
-        )
+        self.bind_slider_spinbox(self.ui.XSlider,self.ui.XEntry,POSITION_SCALE)
+        self.bind_slider_spinbox(self.ui.YSlider,self.ui.YEntry,POSITION_SCALE)
+        self.bind_slider_spinbox(self.ui.ZSlider,self.ui.ZEntry,POSITION_SCALE)
+        self.bind_slider_spinbox(self.ui.rXSlider,self.ui.rXEntry,ROTATION_SCALE)
+        self.bind_slider_spinbox(self.ui.rYSlider,self.ui.rYEntry,ROTATION_SCALE)
+        self.bind_slider_spinbox(self.ui.rZSlider,self.ui.rZEntry,ROTATION_SCALE)
+        self.bind_slider_spinbox(self.ui.TwistSlider,self.ui.TwistEntry,ROTATION_SCALE)
 
     def add_coil(self):
         # creates a new coil
         coil_type = self.ui.TypeDropdown.currentText()
 
-        self.backend.new_coil(
-            np.array([0, 0, 0]),
-            coil_type,
-            1000,
-            False,
-            [0, 0],
-        )
+        self.backend.new_coil(np.array([0, 0, 0]),coil_type,1000,False,[0, 0],)
         self.refresh_list_box()
-
+    
     def import_custom_coil(self):
         # creates a new custom coil
         name = name = self.ui.CustomCoilEntry.text()
 
-        self.backend.new_custom_coil(np.array([0, 0, 0]), name, 1000, False)
+        self.backend.new_custom_coil(np.array([0,0,0]), name, 1000, False)
         self.refresh_list_box()
-
+    
     def delete_selected_coil(self):
         # deletes a coil
         row = self.ui.CoilList.currentRow()
@@ -150,7 +125,7 @@ class Frontend(QMainWindow):
         self.selected_coil_rot = self.backend.get_coil(self.selected_coil_id).rot
         self.ui.stackedWidget.setCurrentWidget(self.ui.EditGUI)
         return
-
+    
     def load_coil_editor(self):
         coil = self.backend.get_coil(self.selected_coil_id)
         self.backend.save_last_coil()
@@ -160,12 +135,7 @@ class Frontend(QMainWindow):
     def edit_coil_position(self):
         if self.updating_gui:
             return
-        self.backend.edit_coil_com(
-            self.selected_coil_id,
-            np.array(
-                [self.ui.XEntry.value(), self.ui.YEntry.value(), self.ui.ZEntry.value()]
-            ),
-        )
+        self.backend.edit_coil_com(self.selected_coil_id,np.array([self.ui.XEntry.value()/1000,self.ui.YEntry.value()/1000,self.ui.ZEntry.value()/1000]))
 
     def edit_coil_dIdt(self):
         if self.updating_gui:
@@ -174,30 +144,21 @@ class Frontend(QMainWindow):
             value = float(self.ui.dIdtEntry.text())
         except ValueError:
             return
-        self.backend.edit_coil_dIdt(self.selected_coil_id, value)
+        self.backend.edit_coil_dIdt(self.selected_coil_id,value)
 
     def edit_coil_rotation(self):
         if self.updating_gui:
             return
         self.refresh_twist()
         self.selected_coil_rot = self.backend.get_coil(self.selected_coil_id).rot
-        self.backend.edit_coil_rot(
-            self.selected_coil_id,
-            np.array(
-                [
-                    self.ui.rXEntry.value(),
-                    self.ui.rYEntry.value(),
-                    self.ui.rZEntry.value(),
-                ]
-            ),
-        )
+        self.backend.edit_coil_rot(self.selected_coil_id,np.array([self.ui.rXEntry.value(),self.ui.rYEntry.value(),self.ui.rZEntry.value()]))
 
     def place_with_white_matter(self):
         if not self.backend.renderer.white_matter_placement_mode:
             self.backend.white_matter_begin()
         else:
             distance = float(self.ui.WhiteMatterDistance.text())
-            self.backend.white_matter_finalize(distance, self.selected_coil_id)
+            self.backend.white_matter_finalize(distance,self.selected_coil_id)
             self.auto_orient()
 
     def auto_orient(self):
@@ -209,9 +170,7 @@ class Frontend(QMainWindow):
     def apply_twist(self):
         if self.updating_gui:
             return
-        self.backend.apply_twist(
-            self.selected_coil_id, self.ui.TwistEntry.value(), self.selected_coil_rot
-        )
+        self.backend.apply_twist(self.selected_coil_id,self.ui.TwistEntry.value(),self.selected_coil_rot)
         self.refresh_coil_editor()
 
     def apply_head_models(self):
@@ -237,7 +196,7 @@ class Frontend(QMainWindow):
         # undoes last operation
         self.backend.undo_operation()
         self.refresh_list_box()
-
+    
     # helpers
     def refresh_coil_editor(self):
         coil = self.backend.get_coil(self.selected_coil_id)
@@ -260,15 +219,10 @@ class Frontend(QMainWindow):
             self.ui.CoilList.addItem(coil.name)
             self.gui_ids.append(coil.id)
         return
-
-    def bind_slider_spinbox(
-        self,
-        slider,
-        spinbox,
-        scale,
-    ):
-        slider.valueChanged.connect(lambda value: spinbox.setValue(value / scale))
-        spinbox.valueChanged.connect(lambda value: slider.setValue(int(value * scale)))
+    
+    def bind_slider_spinbox(self,slider,spinbox,scale,):
+            slider.valueChanged.connect(lambda value: spinbox.setValue(value / scale))
+            spinbox.valueChanged.connect(lambda value: slider.setValue(int(value * scale)))
 
     def on_close(self):
         self.backend.renderer.remove_world_axes()
