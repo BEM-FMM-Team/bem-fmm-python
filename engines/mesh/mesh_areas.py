cbc9a1d624fac27ae7ca3b108ad5a6d3047ee455
PandeCode <pandeshawnbenjamin@gmail.com>
2026-07-22 14:41:56 -0400

    chore: clean up

--- engines/mesh/mesh_areas.py
+++ engines/mesh/mesh_areas.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def mesh_areas(P, t):
+def mesh_areas(P: np.ndarray, t: np.ndarray):
     d12 = P[t[:, 1], :] - P[t[:, 0], :]
     d13 = P[t[:, 2], :] - P[t[:, 0], :]
 
     temp = np.cross(d12, d13)
-    norm = np.linalg.norm(temp, axis=1)
+    norm :np.ndarray= np.linalg.norm(temp, axis=1)
 
     A = 0.5 * norm
     return A.reshape(-1, 1)
