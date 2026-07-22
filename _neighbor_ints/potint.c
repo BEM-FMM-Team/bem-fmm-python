b84c66ce572d55dee4d9d2c89f61d3342fd3ea82
PandeCode <pandeshawnbenjamin@gmail.com>
2026-07-22 13:14:37 -0400

    debug(fix): omp

--- _neighbor_ints/potint.c
+++ _neighbor_ints/potint.c
@@ -213,13 +213,9 @@ void single_layer_potint(
 void c_potint(const size_t N, const double *r1, const double *r2,
               const double *r3, const double *normal, const double *obs,
               double *I, double *Irho) {
-  // perform the calculation in parallel
-  // schedule(static) divides iterations into equal-sized chunks
-  // this is good when all iterations take the same time (which is the case
-  // here)
-
-  // #pragma omp parallel for schedule(static)
-  for (size_t k = 0; k < N; k++) {
+  long k;
+#pragma omp parallel for schedule(static)
+  for (t k = 0; k < N; k++) {
     single_layer_potint(r1[0], r1[1], r1[2], r2[0], r2[1], r2[2], r3[0], r3[1],
                         r3[2], normal[0], normal[1], normal[2], obs[k],
                         obs[k + N], obs[k + 2 * N], &(I[k]), &(Irho[k]),
