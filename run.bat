e1c674950d8aa3d845c72d1fe6239200353dff7f
PandeCode <pandeshawnbenjamin@gmail.com>
2026-07-26 11:05:07 -0400

    move cbemfmm to own repo

---
+++ run.bat
@@ -0,0 +1,15 @@
+
+
+rem check for python
+rem check for python3
+rem check for uv | python -m uv
+rem check for pip | python -m pip
+rem
+rem if uv
+rem then uv run tms_coil_naviagtor
+rem if python -m uv
+rem then python -m uv run tms_coil_naviagtor
+rem
+rem if pip
+rem then python -m pip
+rem then python -m uv run tms_coil_naviagtor
